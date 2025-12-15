7d1f49abc58843f05e91d09e015050c95bcae241
lateshmunde <latesh.munde@cctech.co.in>
2025-12-15 17:25:09 +0530

    working on filehandle

---
+++ Sketcher3D/FileHandle.cs
@@ -0,0 +1,55 @@
+﻿using Sketcher3D.GeometryEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketcher3D
+{
+    internal class FileHandle
+    {
+        public FileHandle() { }
+
+        public static bool SaveToFile(string fileName, List<Shape> shapes)
+        {
+            using (var write = new StreamWriter(fileName))
+            {
+                foreach (var s in shapes)
+                    s.Save(write);     
+            }
+            return true;
+        }
+
+        public static bool SaveToFileGNUPlot(string fileName, List<Shape> shapes)
+        {
+            using (var write = new StreamWriter(fileName))
+            {
+                
+                foreach (var shape in shapes)
+                {
+                    write.WriteLine($"{shape.GetShapeType()}");
+                    write.WriteLine($"{shape.GetShapeName()}");
+                    shape.SaveForGNU(write);
+                }  
+            }
+            return true;
+        }
+
+        public static void ReadSTL(string filename, Triangulation tri)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ');
+
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Sketcher3D/ShapeManager.cs
@@ -0,0 +1,48 @@
+﻿using Sketcher3D.GeometryEngine;
+using System.Collections.Generic;
+
+namespace Sketcher3D
+{
+    internal class ShapeManager
+    {
+        private List<Shape> mShapes;
+        public ShapeManager() { }
+
+        public void AddShape(Shape shape)
+        {
+            mShapes.Add(shape);
+        }
+
+        public List<Shape> GetShapesVec()
+        {
+            return mShapes;
+        }
+
+        public void DeleteShape(int index)
+        {
+            if (mShapes == null) return;
+            if ((index >= 0) && (index < mShapes.Count))
+            {
+                mShapes.RemoveAt(index);
+            }
+        }
+
+        public void DeleteShape()
+        {
+            int index = mShapes.Count - 1;
+            mShapes.RemoveAt(index);
+        }
+
+        public void ClearShape()
+        {
+            mShapes.Clear();
+        }
+
+        public Shape GetLastShape()
+        {
+            if (mShapes == null) return null;
+            return mShapes[mShapes.Count - 1];
+        }
+
+    }
+}
