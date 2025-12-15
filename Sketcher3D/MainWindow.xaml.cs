205a5f10ba8f740a7d79e0816ce4a6b17ec86508
lateshmunde <latesh.munde@cctech.co.in>
2025-12-15 15:19:05 +0530

    transformations migration done

--- Sketcher3D/MainWindow.xaml.cs
+++ Sketcher3D/MainWindow.xaml.cs
@@ -20,9 +20,26 @@ namespace Sketcher3D
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool running = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+            running = !running;
         }
     }
 }
