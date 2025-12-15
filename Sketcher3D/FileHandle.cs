471f421fdee6018562a83390b68bd94738f50791
lateshmunde <latesh.munde@cctech.co.in>
2025-12-15 17:37:21 +0530

    file handling class done

--- Sketcher3D/FileHandle.cs
+++ Sketcher3D/FileHandle.cs
@@ -39,15 +39,49 @@ namespace Sketcher3D
 
         public static void ReadSTL(string filename, Triangulation tri)
         {
+            //List<float> vertices = new List<float>();
+            List<Point> points = new List<Point>();
+
+            Point normal = new Point();
             using (StreamReader reader = new StreamReader(filename))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    string[] words = line.Split(' ');
+                    if (line.Contains("facet normal"))
+                    {
+                        string[] words = line.Split(' ');
+                        float x = float.Parse(words[2]);
+                        float y = float.Parse(words[3]);
+                        float z = float.Parse(words[4]);
 
+                        normal.SetX(x);
+                        normal.SetY(y);
+                        normal.SetZ(z);
+
+                    }
+
+                    if (line.Contains("vertex"))
+                    {
+                        string[] words = line.Split(' ');
+                        float x = float.Parse(words[1]);
+                        float y = float.Parse(words[2]);
+                        float z = float.Parse(words[3]);
+
+                        points.Add(new Point(x, y, z));
+                        if (points.Count == 3)
+                        {
+                            int p1 = tri.GetPointIndex(points[0]);
+                            int p2 = tri.GetPointIndex(points[1]);
+                            int p3 = tri.GetPointIndex(points[2]);
+                            tri.AddTriangle(p1, p2, p3, normal);
+                            points.Clear();
+                        }
+                    }
                 }
             }
+
+
         }
     }
 }
